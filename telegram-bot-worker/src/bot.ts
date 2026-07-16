b5d22e4a5635abd696eadad5e99a02c33b776a57
Nabil Ridhwan <nabridhwan@gmail.com>
2026-07-16 16:46:55 +0800

    feat: improve error handling and streamline CAT status messaging

--- telegram-bot-worker/src/bot.ts
+++ telegram-bot-worker/src/bot.ts
@@ -240,22 +240,15 @@ export function registerHandlers(bot: Bot<Context, Api<RawApi>>, db: Db, runtime
 			}),
 		);
 
-		const loadingMessage = await ctx.reply(LOADING_MESSAGE);
-
-		const message = `CAT Status is unavailable as of 16 July 2026. We're working on a fix. Sorry for any inconvinience caused!`
-
-		await ctx.api.editMessageText(ctx.chat.id, loadingMessage.message_id, message, {
-			parse_mode: 'HTML',
-		});
-
-		const weatherCATResponse = await runtimeEnv.WEATHER_CAT_SERVICE.fetch('https://weather-cat-service/');
-		if (!weatherCATResponse.ok) {
-			throw new Error(`Weather service request failed with status ${weatherCATResponse.status}`);
-		}
-		const readings = (await weatherCATResponse.json()) as WeatherCatResponse;
-		const { cda, httc } = readings;
-
 		try {
+			const weatherCATResponse = await runtimeEnv.WEATHER_CAT_SERVICE.fetch('https://weather-cat-service/');
+			if (!weatherCATResponse.ok) {
+				throw new Error(`Weather service request failed with status ${weatherCATResponse.status}`);
+			}
+
+			const readings = (await weatherCATResponse.json()) as WeatherCatResponse;
+			const { cda, httc } = readings;
+
 			const message = `📍 Civil Defence Academy
 CAT Status: ${cda.catText} ${cda.emoji}
 CAT Start On: ${formatDate(new Date(cda.cat_start_on)) ?? 'N/A'}
@@ -275,7 +268,7 @@ Info last updated: ${formatDate(new Date(cda.update_on)) ?? 'N/A'}
 🟠 CAT 1 (Incoming): CAT 1 has been declared and will take effect at the stated time. Prepare to cease outdoor activities.
 🔴 CAT 1: Heavy rain and/or lightning risk. Outdoor activities are NOT ALLOWED.`;
 
-			await ctx.api.editMessageText(ctx.chat.id, loadingMessage.message_id, message, {
+			return await ctx.reply(message, {
 				parse_mode: 'HTML',
 			});
 		} catch (error) {
@@ -286,11 +279,9 @@ Info last updated: ${formatDate(new Date(cda.update_on)) ?? 'N/A'}
 				}),
 			);
 
-			const message = `There was an error getting the CAT Status. Please try again later.
+			const message = `CAT Status is unavailable right now. Please try again later. Sorry for any inconvenience caused!`;
 
-      Error: ${error}`;
-
-			await ctx.api.editMessageText(ctx.chat.id, loadingMessage.message_id, message, {
+			return await ctx.reply(message, {
 				parse_mode: 'HTML',
 			});
 		}
